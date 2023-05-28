1de800f026e5d5de39b341ac799eadbd3bb1b9e4
Vadim Davidov <vdvdavid21@gmail.com>
2023-05-28 16:18:50 +0300

    Add docker container

--- src/app/page.js
+++ src/app/page.js
@@ -22,3 +22,25 @@ export default function Home() {
     </main>
   );
 }
+
+// * Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// * https://firebase.google.com/docs/web/setup#available-libraries
+
+// * Your web app's Firebase configuration
+// * For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBEqfDBsyV_A0YWSAAUmH44jvJMiTBsndg",
+//   authDomain: "devices-shop.firebaseapp.com",
+//   projectId: "devices-shop",
+//   storageBucket: "devices-shop.appspot.com",
+//   messagingSenderId: "346551305253",
+//   appId: "1:346551305253:web:974c5ac7231e47e135abcc",
+//   measurementId: "G-01FESBH628"
+// };
+
+// * Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
